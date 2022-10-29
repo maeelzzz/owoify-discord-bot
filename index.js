6b036b07c0a758de4708200410c2c6ee42111fd1
maeelzzz <sourisseauma@gmail.com>
2022-10-29 14:13:09 +0200

    🎃

--- index.js
+++ index.js
@@ -44,10 +44,7 @@ client.on(Events.InteractionCreate, async interaction => {
 
 function isValidUrl(userInput) {
     var res = userInput.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
-    if(res == null)
-       return false;
-    else
-       return true;
+    return !(res == null)
 }
 
 async function owoify(url)
